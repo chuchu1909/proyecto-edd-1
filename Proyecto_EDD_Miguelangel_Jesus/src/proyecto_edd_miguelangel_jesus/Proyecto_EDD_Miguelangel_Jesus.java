353a68b5f555d6f11bfec1e71f1858517bf78e39
Miguelangel Maiz <miguelangel.maiz@correo.unimet.edu.ve>
2025-02-23 23:35:20 -0400

    Inicializar Repositorio

---
+++ Proyecto_EDD_Miguelangel_Jesus/src/proyecto_edd_miguelangel_jesus/Proyecto_EDD_Miguelangel_Jesus.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_edd_miguelangel_jesus;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Proyecto_EDD_Miguelangel_Jesus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
